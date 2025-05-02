1770e15d9ae9a87a94d7da959c3ab274426a2b7a
Jorge Barrios <jlbarriossouza@gmail.com>
2025-05-01 22:05:33 -0400

    arreglando nombre user-service

--- auth-service/src/main/java/com/balance/diet/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/balance/diet/auth_service/AuthServiceApplication.java
@@ -3,17 +3,10 @@ package com.balance.diet.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
-@SpringBootApplication
+@SpringBootApplication()
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
-	}
-
-	@PostConstruct
-	public void printEnv() {
-		System.out.println("📛 PROFILE ACTIVO = " + System.getProperty("spring.profiles.active"));
 	}
 }
